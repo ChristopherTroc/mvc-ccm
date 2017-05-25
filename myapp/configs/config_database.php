3778b1bc3ec5720110e8e03ef65978100d28fc5a
Christopher Troc <christopher.troc@gmail.com>
2017-05-25 14:41:57 -0300

    change name db

--- myapp/configs/config_database.php
+++ myapp/configs/config_database.php
@@ -12,7 +12,7 @@
 $config['default']['plugin'] = 'TinyMVC_PDO'; // plugin for db access
 $config['default']['type'] = 'mysql';         // connection type
 $config['default']['host'] = 'localhost';     // db hostname
-$config['default']['name'] = 'ccm';           // db name
+$config['default']['name'] = 'ccmphoto';           // db name
 $config['default']['user']  = 'root';          // db username
 $config['default']['pass'] = 'a1s2d3f4';      // db password
 $config['default']['persistent'] = false;     // db connection persistence?
